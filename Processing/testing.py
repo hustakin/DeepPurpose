151c41a94390fa862c1c44db1973137617ea3526
fanf <fanf@hpc04-gn002.ib0.smc-default.cqu.edu.au>
2022-01-30 16:51:10 +1000

    Finish other training & test.

--- Processing/testing.py
+++ Processing/testing.py
@@ -13,7 +13,7 @@ Testing a given dataset based on a pre-trained model
 '''
 
 
-def batch_predict(path, name, X_drug, X_target, y):
+def batch_predict(path, name, X_drug, X_target, y, repurposing_mode = False):
     print('Loading pre-trained model from path: ' + path)
     model = models.model_pretrained(path)
     drug_encoding = 'CNN'
@@ -21,7 +21,12 @@ def batch_predict(path, name, X_drug, X_target, y):
     test = data_process(X_drug, X_target, y,
                                     drug_encoding, target_encoding,
                                     split_method='no_split')
-    mse, r2, p_val, CI, logits, loss_val = model.predict(test, repurposing_mode = False, test = True)
-    print('[' + name + '] Testing MSE: ' + str(mse) + ' , Pearson Correlation: ' + str(r2) 
-              + ' with p-value: ' + str(f"{p_val:.2E}") +' , Concordance Index: '+str(CI))
-    print('--- Finish Testing ' + name + ' ---')
+    if repurposing_mode:
+        pred = model.predict(test, repurposing_mode = True, test = True)
+        print('--- Finish Predicting ' + str(len(pred)) + ' records ---')
+        return pred
+    else:
+        mse, r2, p_val, CI, logits, loss_val = model.predict(test, repurposing_mode = False, test = True)
+        print('[' + name + '] Testing MSE: ' + str(mse) + ' , Pearson Correlation: ' + str(r2) 
+                  + ' with p-value: ' + str(f"{p_val:.2E}") +' , Concordance Index: '+str(CI))
+        print('--- Finish Testing ' + name + ' ---')
